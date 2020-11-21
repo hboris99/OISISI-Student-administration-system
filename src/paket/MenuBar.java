67f889203430e7e7b7554b07e98351efbb6a000c
panicd9 <panic.ra188.2018@uns.ac.rs>
2020-11-22 00:56:59 +0100

    #status_bar #menu_bar implementirani

--- src/paket/MenuBar.java
+++ src/paket/MenuBar.java
@@ -1,8 +1,16 @@
 package paket;
 
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
 
 public class MenuBar extends JMenuBar {
 
@@ -14,22 +22,62 @@ public class MenuBar extends JMenuBar {
 	public MenuBar() {
 		
 		JMenu file = new JMenu("File");
-		JMenuItem miNew = new JMenuItem("New");
-		JMenuItem miClose = new JMenuItem("Close");
+		
+		JMenuItem miNew = new JMenuItem("New",'N');
+		ImageIcon plus_icon = new ImageIcon(new ImageIcon("images/plus_icon.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT));
+		miNew.setIcon(plus_icon);
+		miNew.setAccelerator(KeyStroke.getKeyStroke("control N"));
+		miNew.addActionListener(new ActionListener() {
+		      public void actionPerformed(ActionEvent e) {
+		        System.out.println("miNew performed");
+		      }
+		});
+
+		JMenuItem miClose = new JMenuItem("Close",'C');
+		ImageIcon x_icon = new ImageIcon(new ImageIcon("images/x_icon.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT));
+		miClose.setIcon(x_icon);
+		miClose.setAccelerator(KeyStroke.getKeyStroke("control C"));
+		miClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("miClose performed");
+			}
+		});
+		
 		file.add(miNew);
 		file.addSeparator();
 		file.add(miClose);
 	
 		JMenu edit = new JMenu("Edit");
-		JMenuItem miEdit = new JMenuItem("Edit");
-		JMenuItem miDelete = new JMenuItem("Delete");
+		
+		JMenuItem miEdit = new JMenuItem("Edit", 'E');
+		ImageIcon edit_icon = new ImageIcon(new ImageIcon("images/edit_icon.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT));
+		miEdit.setIcon(edit_icon);
+		miEdit.setAccelerator(KeyStroke.getKeyStroke("control E"));
+		miEdit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("miEdit performed");
+			}
+		});
+		
+		JMenuItem miDelete = new JMenuItem("Delete", 'D');
+		ImageIcon delete_icon = new ImageIcon(new ImageIcon("images/delete_icon.png").getImage().getScaledInstance(16, 16, Image.SCALE_DEFAULT));
+		miDelete.setIcon(delete_icon);
+		miDelete.setAccelerator(KeyStroke.getKeyStroke("control D"));
 		edit.add(miEdit);
 		edit.addSeparator();
 		edit.add(miDelete);
 		
 		JMenu help = new JMenu("Help");
-		JMenuItem miHelp = new JMenuItem("Help");
-		JMenuItem miAbout = new JMenuItem("About");
+		
+		JMenuItem miHelp = new JMenuItem("Help", 'H');	
+		ImageIcon help_icon = new ImageIcon(new ImageIcon("images/help_icon.png").getImage().getScaledInstance(17, 17, Image.SCALE_DEFAULT));
+		miHelp.setIcon(help_icon);
+		miHelp.setAccelerator(KeyStroke.getKeyStroke("control H"));
+		
+		JMenuItem miAbout = new JMenuItem("About", 'A');
+		ImageIcon about_icon = new ImageIcon(new ImageIcon("images/about_icon.png").getImage().getScaledInstance(17, 17, Image.SCALE_DEFAULT));
+		miAbout.setIcon(about_icon);
+		miAbout.setAccelerator(KeyStroke.getKeyStroke("control A"));
 		help.add(miHelp);
 		help.addSeparator();
 		help.add(miAbout);
---
+++ src/paket/StatusBar.java
@@ -0,0 +1,26 @@
+package paket;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+
+public class StatusBar {
+
+	public StatusBar() {
+		JPanel statusBar = new JPanel(new FlowLayout(FlowLayout.LEFT));
+	    statusBar.setBorder(new CompoundBorder(new LineBorder(Color.DARK_GRAY),
+	        new EmptyBorder(4, 4, 4, 4)));
+	    final JLabel status = new JLabel();
+	    statusBar.add(status);
+
+	    JLabel content = new JLabel("Content in the middle");
+	    content.setHorizontalAlignment(JLabel.CENTER);
+	    
+	}
+}
