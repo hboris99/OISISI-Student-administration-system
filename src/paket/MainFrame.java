cf43eba8db76e66743a22a3b63b758bfccbb81bb
panicd9 <panic.ra188.2018@uns.ac.rs>
2020-11-22 03:21:12 +0100

    Revert "#status_bar #menu_bar implementirani"
    This reverts commit 67f889203430e7e7b7554b07e98351efbb6a000c.

--- src/paket/MainFrame.java
+++ src/paket/MainFrame.java
@@ -1,44 +1,12 @@
 package paket;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
 
 public class MainFrame extends JFrame {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
 	public MainFrame() {
 		
 		MenuBar mb = new MenuBar();
 		this.setJMenuBar(mb);
-		
-		JPanel statusBar = new JPanel();
-	    statusBar.setBorder(new CompoundBorder(new LineBorder(Color.DARK_GRAY),
-	        new EmptyBorder(-3, -2, -3, -2)));
-	    JLabel status = new JLabel("Studentska sluzba");
-	    
-	    JLabel date = new JLabel("datum");
-	    CurrentTime ct = new CurrentTime(date);
-	      
-	    statusBar.setLayout(new BorderLayout());
-	    statusBar.add(status, BorderLayout.WEST);
-	    statusBar.add(date, BorderLayout.EAST);
-	    this.add(statusBar);
-	    
-	    this.setLayout(new BorderLayout());
-	    this.add(statusBar, BorderLayout.SOUTH);
-	    
-	
 	}
 }
