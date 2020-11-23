e8bf65ed395a1ca17b35a001ea46724e8ac3a1a3
Boris Horvat <horvat.boris99@gmail.com>
2020-11-23 03:00:40 +0100

    #predmet_klasa dodata

---
+++ src/paket/Predmet.java
@@ -0,0 +1,27 @@
+package paket;
+
+enum enumSemestar{ZIMSKI,LETNJI};
+public class Predmet {
+	String sifra;
+	String naziv;
+	enumSemestar semestar;
+	int godina_studija;
+	Profesor profesor;
+	int espb;
+	Student[] studentip;
+	Student[] studentinp;
+	
+	Predmet(String sifra, String naziv, enumSemestar semestar, int godina_studija, Profesor profesor,
+			int espb, Student[] studentip, Student[] studentin[])
+	{
+		super();
+		this.sifra = sifra;
+		this.naziv = naziv;
+		this.semestar = semestar;
+		this.godina_studija = godina_studija;
+		this.profesor = profesor;
+		this.espb = espb;
+		this.studentip = studentip;
+		this.studentip = studentinp;
+	}
+}
