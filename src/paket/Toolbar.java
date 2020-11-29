67b203b4edb7e068905348dcba8c713515adcc1a
Boris Horvat <horvat.boris99@gmail.com>
2020-11-29 21:30:49 +0100

    #toolbar dodati actionlisteneri, tooltipovi i mnemonici

--- src/paket/Toolbar.java
+++ src/paket/Toolbar.java
@@ -4,7 +4,11 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -23,28 +27,67 @@ public class Toolbar extends JToolBar{
 		Border emptyBorder = BorderFactory.createEmptyBorder(); 
 
 		
+		
+		
+		
+		
 		JButton addBT = new JButton();
 		addBT.setBorder(emptyBorder);
 		//addBT.setSize(32, 32);
 		ImageIcon plusIcon = new ImageIcon(new ImageIcon("images/plus_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		addBT.setIcon(plusIcon);
+		
+		
+		addBT.setMnemonic('A');
+		addBT.setToolTipText("Dodajte novi entitet");
+		addBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("addBT performed");
+			}
+		});
+		
+		
 		add(addBT);
 		
 		addSeparator();
+		
 		
 		
 		JButton editBT = new JButton();
 		editBT.setBorder(emptyBorder);
 		ImageIcon editIcon = new ImageIcon(new ImageIcon("images/edit_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		editBT.setIcon(editIcon);
+		editBT.setMnemonic('h');
+		editBT.setToolTipText("Izmenite postojeći entitet");
+		editBT.addActionListener(new ActionListener()  {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("editBT performed");
+			}
+		});
 		add(editBT);
 		
 		addSeparator();
 		
 		JButton deleteBT = new JButton();
 		deleteBT.setBorder(emptyBorder);
+		deleteBT.setMnemonic('g');
 		ImageIcon deleteIcon = new ImageIcon(new ImageIcon("images/delete_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		deleteBT.setIcon(deleteIcon);
+		deleteBT.setToolTipText("Obrisite postojeći entitet");
+		deleteBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("deleteBT performed");
+			}
+		});
 		add(deleteBT);
 		
 		Dimension d = new Dimension();
@@ -60,8 +103,18 @@ public class Toolbar extends JToolBar{
 		
 		JButton searchBT = new JButton();
 		searchBT.setBorder(emptyBorder);
+		searchBT.setMnemonic('s');
 		ImageIcon searchIcon = new ImageIcon(new ImageIcon("images/search_icon.png").getImage().getScaledInstance(26, 26,Image.SCALE_SMOOTH));
 		searchBT.setIcon(searchIcon);
+		searchBT.setToolTipText("Pretraži");
+		searchBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("searchBT performed");
+			}
+		});
 		add(searchBT);
 		
 		addSeparator();
