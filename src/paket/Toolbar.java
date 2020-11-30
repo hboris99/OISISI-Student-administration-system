95ed00979f0778885975dda20a40cb50d247d1bd
Boris Horvat <horvat.boris99@gmail.com>
2020-11-30 13:47:43 +0100

    #toolbar promenjeno, nema akceleratore i menmonike shodno onome sto mi je rekao asistent

--- src/paket/Toolbar.java
+++ src/paket/Toolbar.java
@@ -30,30 +30,21 @@ public class Toolbar extends JToolBar{
 
 		
 		
-		
+		addSeparator();
 		
 		
 		JButton addBT = new JButton();
 		addBT.setBorder(emptyBorder);
 		ImageIcon plusIcon = new ImageIcon(new ImageIcon("images/plus_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		addBT.setIcon(plusIcon);
-		/*REFERENCA: https://www.codejava.net/java-se/swing/setting-shortcut-key-and-hotkey-for-menu-item-and-button-in-swing 
-		 Video sam ideju i primenio na ovaj slucaj, jer nisam bio siguran kako se koriste metode getActionMap i get input map 
-		 */
-		Action addBTaction = new AbstractAction("",plusIcon) {
-
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        System.out.println("Addbt preformed");
-		    }
-
-		};
-		addBTaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control N"));
-
-		addBT.getActionMap().put("addBTaction", addBTaction);
-		addBT.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-		        (KeyStroke) addBTaction.getValue(Action.ACCELERATOR_KEY), "addBTaction");
 		
+		addBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("addbt performed");
+			}
+		});
 		addBT.setToolTipText("Dodajte novi entitet");
 		add(addBT);
 		
@@ -66,19 +57,13 @@ public class Toolbar extends JToolBar{
 		ImageIcon editIcon = new ImageIcon(new ImageIcon("images/edit_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		editBT.setIcon(editIcon);
 		
-		Action editBTaction = new AbstractAction("",editIcon) {
-
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        System.out.println("EditBT preformed");
-		    }
-
-		};
-		editBTaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control E"));
-
-		editBT.getActionMap().put("editBTaction", editBTaction);
-		editBT.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-		        (KeyStroke) editBTaction.getValue(Action.ACCELERATOR_KEY), "editBTaction");
+		editBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("editBT performed");
+			}
+		});
 		
 		editBT.setToolTipText("Izmenite postojeći entitet");
 		add(editBT);
@@ -90,25 +75,20 @@ public class Toolbar extends JToolBar{
 		ImageIcon deleteIcon = new ImageIcon(new ImageIcon("images/delete_icon.png").getImage().getScaledInstance(24, 24, Image.SCALE_DEFAULT));
 		deleteBT.setIcon(deleteIcon);
 		
-		Action deleteBTaction = new AbstractAction("",deleteIcon) {
-
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        System.out.println("deleteBT preformed");
-		    }
-
-		};
-		deleteBTaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control D"));
-
-		deleteBT.getActionMap().put("deleteBTaction", deleteBTaction);
-		deleteBT.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-		        (KeyStroke) deleteBTaction.getValue(Action.ACCELERATOR_KEY), "deleteBTaction");
+		deleteBT.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("deleteBT performed");
+			}
+		});
+		
 		
 		deleteBT.setToolTipText("Obrisite postojeći entitet");
 		add(deleteBT);
 		
 		Dimension d = new Dimension();
-		d.setSize(1000, 24);
+		d.setSize(700, 24);
 		addSeparator(d);    //veliki separator 
 		
 		JTextArea tb = new JTextArea();
@@ -122,20 +102,6 @@ public class Toolbar extends JToolBar{
 		searchBT.setBorder(emptyBorder);
 		ImageIcon searchIcon = new ImageIcon(new ImageIcon("images/search_icon.png").getImage().getScaledInstance(26, 26,Image.SCALE_SMOOTH));
 		searchBT.setIcon(searchIcon);
-		
-		Action searchBTaction = new AbstractAction("",searchIcon) {
-
-		    @Override
-		    public void actionPerformed(ActionEvent e) {
-		        System.out.println("searchBT preformed");
-		    }
-
-		};
-		searchBTaction.putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke("control F"));
-
-		searchBT.getActionMap().put("searchBTaction", searchBTaction);
-		searchBT.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-		        (KeyStroke) searchBTaction.getValue(Action.ACCELERATOR_KEY), "searchBTaction");
 		
 		searchBT.setToolTipText("Pretraži");
 		searchBT.addActionListener(new ActionListener() {
