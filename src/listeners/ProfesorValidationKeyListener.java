b8496b79a22ae1991432a276d43f1907c79545ee
Boris Horvat <horvat.boris99@gmail.com>
2021-01-11 00:54:57 +0100

    Dodavanje podataka u ser datoteku i promena par regexa

--- src/listeners/ProfesorValidationKeyListener.java
+++ src/listeners/ProfesorValidationKeyListener.java
@@ -35,14 +35,14 @@ public class ProfesorValidationKeyListener implements KeyListener {
 			b[2] = txt.get(2).getText()
 					.matches("^(?:0[1-9]|[12]\\d|3[01])([\\/.-])(?:0[1-9]|1[012])\\1(?:19|20)\\d\\d$"); // https://stackoverflow.com/questions/22160079/date-of-birth-validation-by-using-regular-expression/22160167
 		if (!txt.get(3).getText().equals(""))
-			b[3] = txt.get(3).getText().matches("([a-zA-Z]+\\s+)+\\d+");
+			b[3] = txt.get(3).getText().matches("^[.0-9a-zA-Z\\s,-]+$");
 		if (!txt.get(4).getText().equals(""))
-			b[4] = txt.get(4).getText().matches("06\\d{8}");
+			b[4] = txt.get(4).getText().matches("0[0-9]{2}\\d{6,7}");
 		if (!txt.get(5).getText().equals(""))
 			b[5] = txt.get(5).getText().matches(
 					"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$");
 		if (!txt.get(6).getText().equals(""))
-			b[6] = txt.get(6).getText().matches("([a-zA-Z]+\\s+)+\\d+");
+			b[6] = txt.get(6).getText().matches("^[.0-9a-zA-Z\\s,-]+$");
 
 		if (!txt.get(7).getText().equals(""))
 			b[7] = txt.get(7).getText().matches("^[0-9]{9}$");
